5fdc308f5bde73fb0785b3527597dc4ea02dbe40
Malli Gelli <malleswari.gelli@gmail.com>
2024-02-27 10:09:05 -0600

    added

--- demo.py
+++ demo.py
@@ -2,4 +2,13 @@
 
 from US_Visa.logger import logging
 
-logging.info("welcome to our custom log")
+from US_Visa.exception import USvisaException
+
+import sys
+
+try:
+    a = 1/"10"
+    
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e,sys) from e
