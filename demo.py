bdcd0eacd63fbaad712fdaeba978a3693be13596
Malli Gelli <malleswari.gelli@gmail.com>
2024-02-27 13:10:42 -0600

    updated

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
-'''
 
 from US_Visa.logger import logging
 
 from US_Visa.exception import USvisaException
 
 import sys
+logging.info("Enter logging")
 
 try:
     a = 1/"10"
@@ -13,4 +13,4 @@ except Exception as e:
     logging.info(e)
     raise USvisaException(e,sys) from e 
     
-'''
+
