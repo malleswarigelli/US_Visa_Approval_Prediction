a8c271f213f75d0a8d60ebd53d8f921437fae2ef
Malli Gelli <malleswari.gelli@gmail.com>
2024-02-27 21:58:37 -0600

    Feature eng added

--- US_Visa/logger/__init__.py
+++ US_Visa/logger/__init__.py
@@ -1,23 +1,22 @@
-
 import logging
 import os
 
 from from_root import from_root
 from datetime import datetime
 
-
-
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+print('log_dir:',LOG_FILE)
+
 log_dir = "logs"
 
 logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
 
-os.makedirs(logs_path, exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
+print('log_dir:',log_dir)
 
-
-logging.basicConfig(    
+logging.basicConfig(
     filename=logs_path,
-    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s", 
     level=logging.DEBUG,
+    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    
 )
-
--- demo.py
+++ demo.py
@@ -1,16 +1,11 @@
 
 from US_Visa.logger import logging
-
 from US_Visa.exception import USvisaException
-
 import sys
-logging.info("Enter logging")
 
+logging.info("Logging file working")
 try:
-    a = 1/"10"
-    
+    a = 1 / "10"
 except Exception as e:
     logging.info(e)
-    raise USvisaException(e,sys) from e 
-    
-
+    raise USvisaException(e, sys) from e
