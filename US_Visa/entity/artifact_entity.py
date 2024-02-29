732c77a27984a6a4f9a7a626df294a76bf1352aa
Malli Gelli <malleswari.gelli@gmail.com>
2024-02-29 15:13:32 -0600

    DataIngestionArtifact upated

---
+++ US_Visa/entity/artifact_entity.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass # with this, you can declare class variables without needing self keyword
+class DataIngestionArtifact:
+    trained_file_path:str 
+    test_file_path:str 
