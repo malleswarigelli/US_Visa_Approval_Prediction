cecf2e2689a593cc93bed6220806194092d255c9
Malli Gelli <malleswari.gelli@gmail.com>
2024-03-06 17:08:20 -0600

    components, pipelines app.py updated

--- US_Visa/configuration/aws_connection.py
+++ US_Visa/configuration/aws_connection.py
@@ -1,2 +1,40 @@
 
 # Connect with S3 bucket with help of AWS secret key and access key using boto3 package
+
+import boto3
+import os
+from US_Visa.constant import AWS_SECRET_ACCESS_KEY_ENV_KEY, AWS_ACCESS_KEY_ID_ENV_KEY, REGION_NAME
+
+
+class S3Client:
+
+    s3_client=None
+    s3_resource = None
+    def __init__(self, region_name=REGION_NAME):
+        """ 
+        This Class gets aws credentials from env_variable and creates an connection with s3 bucket 
+        and raise exception when environment variable is not set
+        """
+
+        if S3Client.s3_resource==None or S3Client.s3_client==None:
+            __access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY, )
+            __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY, )
+            if __access_key_id is None:
+                raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not set.")
+            if __secret_access_key is None:
+                raise Exception(f"Environment variable: {AWS_SECRET_ACCESS_KEY_ENV_KEY} is not set.")
+
+            # access class variable i.e class.varname (no need to create object)
+            S3Client.s3_resource = boto3.resource('s3',
+                                            aws_access_key_id=__access_key_id,
+                                            aws_secret_access_key=__secret_access_key,
+                                            region_name=region_name
+                                            )
+            S3Client.s3_client = boto3.client('s3',
+                                        aws_access_key_id=__access_key_id,
+                                        aws_secret_access_key=__secret_access_key,
+                                        region_name=region_name
+                                        )
+        self.s3_resource = S3Client.s3_resource
+        self.s3_client = S3Client.s3_client
+        
--- US_Visa/constant/__init__.py
+++ US_Visa/constant/__init__.py
@@ -63,4 +63,10 @@ MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 """
 MODEL EVALUATION related constant start with MODEL_EVALUATION var name
 """
-MODEL_EVALUATION_DIR_NAME: str = "model_evaluation"
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisamodel2024"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
